059fdae31eef27e298dcef0998e5f5b013484983
HappySmile <153109864+devmad001@users.noreply.github.com>
2024-07-05 10:27:36 -0400

    ini

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Editor from './components/Editor';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Editor/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
