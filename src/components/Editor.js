a9bc0d03f348945f8385788191515fb3d57b336a
devmad001 <shinnysmile0901@gmail.com>
2024-07-06 02:33:23 +0900

    list MVP completed

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Footer from './Footer';
 
 function Editor() {
   const [checked, setChecked] = React.useState([]);
@@ -45,34 +45,36 @@ function Editor() {
                     />
                 </ListItemIcon>
                 <div className={checked.indexOf(value) !== -1 ? "light" : "dark"}>
-                    <ListItemButton onClick={() => {
+                    <Box component="section" sx={{ p: 2 }} onClick={() => {
                         setValue(value);
                     }}>
-                        <div className='gap'></div>
                         {
                             editvalue != value ?
-                            <Grid container spacing={2}>
-                                <Grid item xs={6}>
-                                    <div>Created BI applications with QlikView, Tableau, and Power BI for marketing, customer acquisition, CRM, responsible gaming, payments, and finance.</div>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={6}>
+                                        <div>Created BI applications with QlikView, Tableau, and Power BI for marketing, customer acquisition, CRM, responsible gaming, payments, and finance.</div>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <div>Création d'applications BI avec QlikView, Tableau et Power BI pour le marketing, l'acquisition de clients, le CRM, les jeux responsables, les paiements et les finances.</div>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={6}>
-                                    <div>Création d'applications BI avec QlikView, Tableau et Power BI pour le marketing, l'acquisition de clients, le CRM, les jeux responsables, les paiements et les finances.</div>
-                                </Grid>
-                            </Grid>
                             : <Grid container spacing={2}>
                                 <Grid item xs={6}>
                                     <div>Created BI applications with QlikView, Tableau, and Power BI for marketing, customer acquisition, CRM, responsible gaming, payments, and finance.</div>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <textarea rows={5} className='textarea'>Création d'applications BI avec QlikView, Tableau et Power BI pour le marketing, l'acquisition de clients, le CRM, les jeux responsables, les paiements et les finances.</textarea>
+                                    <textarea rows={5} className='textarea' defaultValue={"Création d'applications BI avec QlikView, Tableau et Power BI pour le marketing, l'acquisition de clients, le CRM, les jeux responsables, les paiements et les finances."}></textarea>
                                     <div style={{ "float": "right" }}>
                                         <Button variant="contained" style={{ "marginRight": "2px" }}>T+>></Button>
                                         <Button variant="contained">TRANSLATED</Button>
                                     </div>
                                 </Grid>
+                                <Grid item xs={12}>
+                                    <Footer/>
+                                </Grid>
                             </Grid>
                         }
-                    </ListItemButton>
+                    </Box>
                 </div>
             </ListItem>
             );
